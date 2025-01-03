0bcb448b67141d095b40a325def7ea362efc88da
Yersa Nordman <finwo@pm.me>
2025-01-03 21:23:35 +0100

    createContext passthrough return type

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { AsyncLocalStorage } from 'node:async_hooks';
 
 const context = new AsyncLocalStorage();
 
-type Callable = () => unknown;
-type Context  = Record<string, any>;
+type Callable<T> = () => T;
+type Context     = Record<string, any>;
 
 export function getContext(): Context {
   return context.getStore() as Context;
 };
 
-export function createContext(ctx: Context, handler: Callable) {
+export function createContext<T = unknown>(ctx: Context, handler: Callable<T>) {
   return context.run(ctx, handler);
 };
 
